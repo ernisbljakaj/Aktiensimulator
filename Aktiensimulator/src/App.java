600d55d6c38a66536f770ea639e1a51475ae8e96
Ernis Bljakaj <90210biolem@gmail.com>
2026-09-10 16:05:21 +0200

    Created a HashMap

--- Aktiensimulator/src/App.java
+++ Aktiensimulator/src/App.java
@@ -1,5 +1,24 @@
+
+import java.util.HashMap;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        HashMap<Integer, String> map = new HashMap<>();
+
+        map.put(100, "0");
+        map.put(101, "0");
+        map.put(102, "1");
+        map.put(103, "0");
+        map.put(104, "0");
+
+        map.put(200, "1");
+        map.put(201, "1");
+        map.put(202, "1");
+        map.put(203, "1");
+        map.put(204, "1");
+
+        
+        System.out.println(map);
+
     }
 }
